40213d11fed57a411741d0e4d50b9ad3b1f7653d
Kan Ouivirach <zkan.cs@gmail.com>
2023-01-10 20:04:13 +0700

    feat: Get buzz when input is 10
    Test Plan: input 5 and see if output is buzz
    
    Reviewers: 
    
    Pull Request: https://github.com/zkan/hello-spr/pull/14

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,7 @@ def fizzbuzz(number: int) -> str:
     if number % 3 == 0:
         return "Fizz"
 
-    if number == 5:
+    if number == 5 or number == 10:
         return "Buzz"
 
     return str(number)
