b8917fbd2d819194b29bcc3e544631fff968313f
Kan Ouivirach <zkan.cs@gmail.com>
2023-01-12 20:36:01 +0700

    feat: Get FizzBuzz when number is divisible by 3 and 5
    Test Plan: test
    
    Reviewers: 
    
    Pull Request: https://github.com/zkan/hello-spr/pull/20

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,11 @@
 def fizzbuzz(number: int) -> str:
-    if number == 15:
+    if number % 3 == 0 and number % 5 == 0:
         return "FizzBuzz"
-
-    if number % 3 == 0:
-        return "Fizz"
 
     if number % 5 == 0:
         return "Buzz"
+
+    if number % 3 == 0:
+        return "Fizz"
 
     return str(number)
